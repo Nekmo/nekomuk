333df2af91807b5ce63e1f62f2eb257ada283c55
Nekmo <contacto@nekmo.com>
2011-10-07 12:47:37 +0200

    Primer commit con labels para GNU/Linux y comenzado main

---
+++ nekomuk/labeldevice/linux.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+
+class LabelDevice(object):
+    devices = {}
+    def __init__(self):
+        pattern = r"([^ +]) on (.+) type ([^ ]+) \((.+)\)(?: \[(.+)\]|)"
+        lines = os.popen('mount -l').readlines()
+        for line in lines:
+            data = re.findall(pattern, line)[0]
+            self.devices[data[1]] = {
+                'filesystem': data[0],
+                'type': data[2],
+                'flags': data[3],
+                'label': data[4],
+            }
+    def get_device_by_path(self, path):
+        for device in sorted(self.devices.keys(), key=len, reverse=True):
+            if path.startswith(device):
+                return self.devices[device]
+        
